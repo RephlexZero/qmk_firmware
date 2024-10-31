bd7b29e1baedd8c2f286d3a6cfdeeb29c30ca1d0
RephlexZero <rephlexzero@protonmail.com>
2024-10-31 22:29:28 +0000

    Lut fix

--- keyboards/rephlex/moonboard/lut.c
+++ keyboards/rephlex/moonboard/lut.c
@@ -4,6 +4,7 @@ SPDX-License-Identifier: GPL-2.0-or-later */
 #include "scanfunctions.h"
 #include "util.h"
 
+// https://www.desmos.com/calculator/zmk9zwvdg7
 
 /* Equation parameters for the sensor-magnet linearity mapping */
 const double lut_a = 0.200347177016; // Latenpow
@@ -18,10 +19,11 @@ uint16_t distance_to_adc(uint8_t distance) {
 }
 
 uint8_t adc_to_distance(uint16_t adc) {
-    if (adc <= lut_d) {
+    double check = (adc - lut_d) / lut_a;
+    if (check <= 0) {
         return 0;
     }
-    double intermediate = (log((adc - lut_d) / lut_a) - lut_c) / lut_b;
+    double intermediate = (log(check) - lut_c) / lut_b;
     uint8_t distance = (uint8_t) MAX(0, MIN(intermediate, 255));
     return distance;
 }
